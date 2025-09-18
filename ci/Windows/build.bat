f6864c6f40402b3e13ad1ee540c880c39ee9be35
michaelrossherron <michaelrossherron@gmail.com>
2025-09-18 14:24:06 -0600

    Updated windows build script [skip ci]

--- ci/Windows/build.bat
+++ ci/Windows/build.bat
@@ -1,6 +1,6 @@
 call "%ONEAPI_ROOT%\setvars.bat"
 set /a "PROC=%NUMBER_OF_PROCESSORS%"
-cmake -version
+cmake --version
 
 SET STARTDIR=%cd%
 SET SCRIPTDIR=%~dp0
@@ -8,10 +8,13 @@ SET SCRIPTDIR=%~dp0
 cd %SCRIPTDIR%\..\..\
 
 if not defined FEBIO_SDK set "FEBIO_SDK=%STARTDIR%\febio4-sdk"
+set "FEBIO_SDK=%FEBIO_SDK:\=/%"      :: convert backslashes to forward slashes
+set "BUILD_DIR=%CD%/cmbuild"
 
-cmake -S . -B "%CD%\cmbuild" -DFEBio_SDK=%FEBIO_SDK%
+cmake -S . -B "%BUILD_DIR%" -DFEBio_SDK=%FEBIO_SDK%
 
-cd cmbuild
+cd "%BUILD_DIR%"
 msbuild /P:Configuration=Release /P:WarningLevel=0 /m:%PROC% ALL_BUILD.vcxproj
-cd %STARTDIR%
+
+cd "%STARTDIR%"
 exit 0
